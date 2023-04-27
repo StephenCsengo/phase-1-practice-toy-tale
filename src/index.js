c90f79698228cdbb045b82019bedee29e29b0805
Stephen Csengo <stephencsengo@gmail.com>
2023-04-27 11:50:50 -0400

    Cards displaying

--- src/index.js
+++ src/index.js
@@ -12,4 +12,46 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  //Add toy cards
+  fetch("http://localhost:3000/toys")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(renderCards)
 });
+
+function renderCards(cards) {
+  const toyCollection = document.getElementById("toy-collection");
+  cards.forEach(data => {
+    //Create a div for each card
+    const card = document.createElement("div");
+    card.classList.add("card");
+    //Add title
+    const h2 = document.createElement("h2");
+    h2.innerText = data.name;
+    //Add image
+    const img = document.createElement("img");
+    img.src = data.image;
+    img.classList.add("toy-avatar");
+    //Add like counter
+    const p = document.createElement("p");
+    p.innerText = `${data.likes} Likes`;
+    //Add like button
+    const btn = document.createElement("button");
+    btn.innerText = "Like ❤️";
+    btn.classList.add("like-btn");
+    //Append all to card and append card to toyCollection
+    card.append(h2, img, p, btn);
+    toyCollection.appendChild(card);
+  })
+}
+
+function handleSumbit(e) {
+  e.preventDefault();
+  let toyObj = {
+    name:e.target.name.value,
+    image:e.target.image.value,
+    likes:0
+  }
+}
